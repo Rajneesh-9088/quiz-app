0e862be6a63cd91c4768f29a317e8e78844e7c67
Rajneesh-9088 <rp8735685@gmail.com>
2021-11-21 15:08:45 +0530

    inserted data into admin and user

---
+++ add_admin.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="QUIZ"
+)
+
+mycursor = mydb.cursor()
+
+n = input("Enter Name: ")
+u = input("Enter Username:  ")
+p = input("Enter Password:  ")
+
+sql = 'INSERT into admin (name,username, password) VALUES(%s,%s,%s)'
+val = (n, u, p)
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted")
