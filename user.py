8afa9da20f6d693b8086c866652e8ec1b6b92aab
Rajneesh-9088 <rp8735685@gmail.com>
2021-11-21 14:48:17 +0530

    created admin and user tables

---
+++ user.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="",
+    database="QUIZ"
+)
+
+mycursor = mydb.cursor()
+
+sql = "CREATE TABLE user(userid int(5) NOT NULL AUTO_INCREMENT, name VARCHAR(100) NOT NULL, username VARCHAR(100) NOT NULL, password VARCHAR(100) NOT NULL, PRIMARY KEY(userid))"
+
+mycursor.execute(sql)
+mycursor.execute("SHOW TABLES")
